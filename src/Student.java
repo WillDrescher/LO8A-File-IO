b940544d0b61e69c9ada41fc581a90bc4753b56a
Will Drescher <will_drescher@mines.edu>
2026-03-01 20:02:46 -0700

    just starting

---
+++ src/Student.java
@@ -0,0 +1,29 @@
+public class Student implements Comparable<Student> {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+
+    @Override
+    public int compareTo(Student other) {
+        return this.name.compareTo(other.name);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%-15s %3d", name, score);
+    }
+}
